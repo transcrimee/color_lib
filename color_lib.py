6759f16ec87548274af5ae4ad3a735a0891e4dbd
Transcrime <107689370+transcrimee@users.noreply.github.com>
2026-01-12 17:04:26 -0400

    Add files via upload

--- color_lib.py
+++ color_lib.py
@@ -2,4 +2,10 @@ class color:
     @staticmethod
     def rgb_text(r, g, b, text):
         return f"\033[38;2;{r};{g};{b}m{text}\033[0m"
-
+class style:
+    @staticmethod
+    def text_style(b, text):
+     return f"\033[1;{b}m{text}\033[0m"
+        
+print(color.rgb_text(100, 255, 100, "This is custom green text"))
+print(style.text_style(1, "This is custom bold text"))
